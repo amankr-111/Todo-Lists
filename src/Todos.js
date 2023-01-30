5773f701bc691c90d721d5bf8571abde4275f3b0
Aman <amankr0741@gmail.com>
2023-01-30 19:17:42 +0530

    work on delete button compleated

--- src/Todos.js
+++ src/Todos.js
@@ -2,7 +2,29 @@ import React, { useState } from 'react'
 import './style.css'
 const Todos = () => {
   const [inputData, setInputData] = useState('')
-  comst[input,setInput]=useState([])
+  const [input,setInput]=useState([])
+  const addItem=()=>{
+    if(!inputData){
+      alert('please fill the data')
+    }
+    else{
+      const myNewInputData={
+        id:new Date().getTime().toString(),
+        name:inputData,
+      }
+    setInput([...input,myNewInputData ])
+    setInputData('')
+  }
+  }
+  const deleteItem=(index)=>{
+    const updatedItems=input.filter((cerElem)=>{
+      return cerElem.id !== index;
+    })
+    setInput(updatedItems)
+  }
+const  deleteAll=()=>{
+      setInput([])
+  }
   return (
     <div className='main-div'>
       <div className='child-div'>
@@ -20,19 +42,20 @@ const Todos = () => {
            onChange={(event) =>setInputData(event.target.value)}>
 
            </input>
-        <i className="fa fa-plus add-btn" onClick={setState}'></i>
+        <i className="fa fa-plus add-btn" onClick={addItem}></i>
       </div>
-
+          
       <div className='showItems' >
-        <div className='eachItem'>
-          <h3>Apple</h3>
-          <div className='todo-btn'>
-            <i className="far fa-edit add-btn"></i>
-            <i className="far fa-trash-alt add-btn"></i>
-          </div>
-        </div>
-
-        <button className='btn effect04' data-sm-link-text='Remove All'>
+      {input.map((cerElem)=>{
+              return( <div className='eachItem' key={cerElem.id}>
+              <h3>{cerElem.name}</h3>
+              <div className='todo-btn'>
+                <i className="far fa-edit add-btn"></i>
+                <i className="far fa-trash-alt add-btn" onClick={()=>{deleteItem(cerElem.id)}}></i>
+              </div>
+            </div>)
+          })}
+        <button className='btn effect04' data-sm-link-text='Remove All' onClick={deleteAll}>
           <span>check List</span>
         </button>
       </div>
